031d22521095206158578c0440a1f1b61d8f9dbc
deimos <luigi.benvenuto01@gmail.com>
2020-07-04 12:38:31 +0200

    connessione a mysql aggiunta libreria marshmallow

--- mysql.py
+++ mysql.py
@@ -1,28 +1,41 @@
 from flask import Flask,jsonify,request
-from flask_mysqldb import MySQL
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
-import pymysql
-
-
+from flask_marshmallow import Marshmallow
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'some secret string here'
-app.config['SQLALCHEMY_DATABASE_URI'] = "mysql://root:''@localhost:3306/todo"
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql://root:123456@localhost:3306/todo"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
 db = SQLAlchemy(app)
 CORS(app)
+ma = Marshmallow(app)
+
 
 class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String, unique=True, nullable=False)
 
+    def __init__(self,id,title):
+        self.id = id
+        self.title=title
+
+class ProductSchema(ma.Schema):
+    class Meta:
+        fields = ('id','title')
+
+
+product_schema = ProductSchema()
+product_schema = ProductSchema(many=True)
+
+
 
 @app.route('/api/tasks', methods=['GET'])
 def get_all():
-    user = Task.query.all()
-    return jsonify(user)
+    all_tasks = Task.query.all()
+    result = product_schema.dump(all_tasks)
+    return jsonify(result)
 
 @app.route('/prova', methods=['GET'])
 def get():
