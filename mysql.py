70e0322f82e407afa7f316ab31eeb3d7c197444f
deimos <luigi.benvenuto01@gmail.com>
2020-07-09 18:44:59 +0200

    connessione a MySql

--- mysql.py
+++ mysql.py
@@ -2,10 +2,15 @@ from flask import Flask,jsonify,request
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
+import pymysql
+
 app = Flask(__name__)
+connection = pymysql.connect(host="localhost",user="root",passwd="manager",database="todo" )
+cursor = connection.cursor()
 
 app.config['SECRET_KEY'] = 'some secret string here'
-app.config['SQLALCHEMY_DATABASE_URI'] = "mysql://root:123456@localhost:3306/todo"
+#app.config['SQLALCHEMY_DATABASE_URI'] = userpass + basedir + dbname
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:manager@localhost/todo"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
 db = SQLAlchemy(app)
@@ -17,8 +22,7 @@ class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String, unique=True, nullable=False)
 
-    def __init__(self,id,title):
-        self.id = id
+    def __init__(self,title):
         self.title=title
 
 class ProductSchema(ma.Schema):
@@ -37,9 +41,17 @@ def get_all():
     result = product_schema.dump(all_tasks)
     return jsonify(result)
 
-@app.route('/prova', methods=['GET'])
-def get():
-    return jsonify("strunz")
+
+@app.route('/api/task', methods=['POST'])
+def insert_task():
+    title = request.json['title']
+
+    new_task = Task(title)
+    db.session.add(new_task)
+    db.session.commit()
+
+    return jsonify(new_task)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
